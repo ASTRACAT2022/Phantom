afb88cab93c861b996a6846127ba290353cb9056
ASTRACAT2022 <ASTRACAT2022@users.noreply.github.com>
2026-03-15 01:21:10 +0300

    chore: optimize sysctl for gigabit throughput

--- node-controller/install-fptn-node.sh
+++ node-controller/install-fptn-node.sh
@@ -298,6 +298,8 @@ net.core.rmem_max = 67108864
 net.core.wmem_max = 67108864
 net.ipv4.tcp_rmem = 4096 87380 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_notsent_lowat = 16384
 EOF
 
   modprobe tcp_bbr 2>/dev/null || true
